84fbe957c9b6f234d26c945b126435b6ccd9cdcc
Soulaimane Bouabid <sbouabid@e3r1p8.1337.ma>
2024-07-25 08:46:55 +0100

    add ex02

--- ex02/Animal.hpp
+++ ex02/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: sbouabid <sbouabid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 14:15:48 by sbouabid          #+#    #+#             */
-/*   Updated: 2024/07/24 16:46:31 by sbouabid         ###   ########.fr       */
+/*   Updated: 2024/07/25 08:19:41 by sbouabid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ class	Animal
 		Animal(Animal &other);
 		Animal &operator=(const Animal &other);
 		virtual ~Animal();
-		virtual	void	makeSound() const;
+		virtual	void	makeSound() const = 0;
 		virtual	std::string	getType() const;
 };
 
